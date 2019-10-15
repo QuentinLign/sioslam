a0059e043cac9420c6dca4f3a41f28319c386e2c
QuentinLign <48805997+QuentinLign@users.noreply.github.com>
2019-10-15 11:26:46 +0200

    Add files via upload

---
+++ Exercice 1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int n, i, tab[6];
+
+	for (i=0 ; i<7; i = i + 1)
+	{
+		printf("Entrez element %d ", i);
+		scanf("%d", &tab[i]);
+		
+	}	
+	
+	for (i=0 ; i<7; i = i + 1)
+	{
+		printf("element %d vaut %d\n ", i, tab[i]);		
+		
+	}
+	return 0;
+	
+}
